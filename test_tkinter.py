eca88a41984edbecc23e4b6abc7b3316ff295a32
Bryan Untalan <44619836+buntalan@users.noreply.github.com>
2024-04-15 11:18:12 -0500

    Added tkinter window with Text widget. Surrounded text with paddding.

---
+++ test_tkinter.py
@@ -0,0 +1,12 @@
+    #    label = tk.Label(window, text="Testing this out.")
+    #     frame = tk.Frame(window)
+    #     inp = tk.Text(frame, height=24)
+
+    #     window.geometry("800x800")
+    #     window.columnconfigure(1, weight=10)
+    #     window.rowconfigure(1, weight=30)
+
+    #     label.grid(row=1, column=0)
+    #     frame.grid(row=0, column=1, sticky="we")
+    #     inp.grid()
+    #     tk.Entry(window).grid(row=2, column=2)
