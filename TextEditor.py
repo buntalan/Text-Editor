9c65be2167b10a355f92fd02e0715ad23fa7997c
Bryan Untalan <44619836+buntalan@users.noreply.github.com>
2024-04-15 12:53:08 -0500

    added scrollbar, need to fix scrolling with window.

--- TextEditor.py
+++ TextEditor.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.scrolledtext as scrolledtext
 
 """Imitation program of the text editor for windows."""
 
@@ -7,16 +8,28 @@ class TextEditor():
         """Initialize Text Editor"""
         window = tk.Tk()
         window.title("Text Editor")
+        window.grid_propagate(False)
+        window.grid_rowconfigure(0, weight=1)
+        window.grid_columnconfigure(0, weight=1)
+        window.geometry("1200x720")
 
-        text_widget = tk.Text(window, 
-                              wrap=tk.WORD, 
-                              maxundo=100, 
-                              padx=10, 
-                              pady=10
-                              )
+        # Create ScrolledText within window
+        st = scrolledtext.ScrolledText(window)
 
-        text_widget.grid(row=0, column=0, sticky=tk.NSEW)
+        # Create text widget to contain user keyboard input
+        text = tk.Text(window, 
+                       wrap=tk.WORD, 
+                       maxundo=100, 
+                       padx=10, 
+                       pady=10,
+                       )
         
+        # Add vertical scroll bar (vsb)
+        vsb = tk.Scrollbar(window, orient=tk.VERTICAL, command=text.yview)
+        text['yscrollcommand'] = vsb.set
+
+        vsb.grid(row=0, column=1, sticky=tk.E)
+        text.grid(row=0, column=0, sticky=tk.NSEW)
 
         window.mainloop()
 
@@ -25,6 +38,10 @@ class TextEditor():
 
     def create_menu(self):
         """Create drop down menu for text editor"""
+        pass
+
+    def create_scrollbar(self, window):
+        """Create scrollbar that appears when text exceeds window. """
         pass
 
     def save(self):
