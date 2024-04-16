668d4b710aea694e373476570b11456cf15688f0
PrivRyan <44619836+PrivRyan@users.noreply.github.com>
2024-04-16 11:30:26 -0500

    added file menu ribbon

--- TextEditor.py
+++ TextEditor.py
@@ -16,7 +16,7 @@ class TextEditor():
 
         # Create text widget to contain user keyboard input
         text = tk.Text(window, 
-                       wrap=tk.WORD, 
+                       wrap=tk.CHAR, 
                        maxundo=100, 
                        padx=10, 
                        pady=10,
@@ -27,29 +27,86 @@ class TextEditor():
         vsb = tk.Scrollbar(window, orient=tk.VERTICAL, command=text.yview)
         text['yscrollcommand'] = vsb.set
 
+        # Add horizontal scroll bar (hsb) and tie widget horizontal
+        # scroll to HSB
+        hsb = tk.Scrollbar(window, orient=tk.HORIZONTAL, command=text.xview)
+        text['xscrollcommand'] = hsb.set
+
         # Create menu widget
-        menubar = tk.Menu(window)
+        menubar = tk.Menu(window, tearoff=0)
         window['menu'] = menubar
-        menu_file = tk.Menu(menubar)
-        menu_edit = tk.Menu(menubar)
+
+        # Create menu items and add to menubar
+        menu_file = tk.Menu(menubar, tearoff=0)
+        menu_edit = tk.Menu(menubar, tearoff=0)
+        menu_format = tk.Menu(menubar, tearoff=0)
+        menu_view = tk.Menu(menubar, tearoff=0)
+        menu_help = tk.Menu(menubar, tearoff=0)
+
         menubar.add_cascade(menu=menu_file, label="File")
         menubar.add_cascade(menu=menu_edit, label="Edit")
+        menubar.add_cascade(menu=menu_format, label="Format")
+        menubar.add_cascade(menu=menu_view, label="View")
+        menubar.add_cascade(menu=menu_help, label="Help")
+
+        # Add File menu items
+        menu_file.add_command(label="New", command=self.new)
+        menu_file.add_command(label="New Window", command=self.new_window)
+        menu_file.add_command(label="Open", command=self.open)
+        menu_file.add_command(label="Save", command=self.save)
+        menu_file.add_command(label="Save As...", command=self.save_as)
+        menu_file.add_separator()
+        menu_file.add_command(label="Page Setup", command=self.page_setup)
+        menu_file.add_command(label="Print", command=self.print)
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit", command=self.exit)
+
+        # Add edit menu items
+        menu_edit.add_command(label="Undo", command=self.undo)
+        menu_edit.add_separator()
+        menu_edit.add_command(label="Cut")
+        menu_edit.add_command(label="Copy")
+        menu_edit.add_command(label="Paste")
+        menu_edit.add_command(label="Delete")
+        menu_edit.add_separator()
+        menu_edit.add_command(label="Select All")
+        menu_edit.add_command(label="Time/Date")
+
+        # Add format menu items
+        menu_format.add_checkbutton(label="Word Wrap")
+        menu_format.add_command(label="Font...")
+
+        # Add view menu items
+        menu_zoom = tk.Menu(menu_view, tearoff=0)
+        menu_zoom.add_command(label="Zoom In")
+        menu_zoom.add_command(label="Zoom Out")
+        menu_zoom.add_command(label="Restore Default Zoom")
+        menu_view.add_cascade(menu=menu_zoom, label="Zoom")
+        menu_view.add_checkbutton(label="Status Bar")
+        
+        # Add help menu item
+        menu_help.add_command(label="About Text Editor")
 
         # Add widgets to frame
         vsb.grid(row=0, column=1, sticky=tk.NS)
+        hsb.grid(row=1, column=0, sticky=tk.EW)
         text.grid(row=0, column=0, sticky=tk.NSEW)
 
         window.mainloop()
 
     def run(self):
         """Loop and respond to keyboard and mouse events"""
-
-    def create_menu(self):
-        """Create drop down menu for text editor"""
+        
+    def new(self):
+        """Save text file"""
         pass
 
-    def create_scrollbar(self, window):
-        """Create scrollbar that appears when text exceeds window. """
+    def new_window(self):
+        """Save text file"""
+        pass
+    
+    def open(self):
+        """Open saved text file"""
         pass
 
     def save(self):
@@ -60,10 +117,21 @@ class TextEditor():
         """Save file as into directory of user's choice"""
         pass
 
-    def open(self):
-        """Open saved text file"""
+    def page_setup(self):
+        """Go through windows print page dialog and configure print"""
         pass
 
+    def print(self):
+        """Send text to printer"""
+        pass
+
+    def exit(self):
+        """Exit text editor"""
+        pass
+
+    def undo(self):
+        """Undo last edit"""
+        pass
 
 if __name__ == "__main__":
     te = TextEditor()
